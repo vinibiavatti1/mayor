e5ffe30fde9dbb1d08c56232d30d06390ceb01fa
vinibiavatti1 <vinicius.biavatti@gmail.com>
2026-02-11 19:25:34 +0000

    Mayor!

--- src/main/java/com/vinibiavatti1/mayor/Constants.java
+++ src/main/java/com/vinibiavatti1/mayor/Constants.java
@@ -2,9 +2,7 @@ package com.vinibiavatti1.mayor;
 
 public class Constants {
     public static final String TITLE = "Mayor";
-    public static final String VERSION = "1.0.0";
-
-
+    public static final String VERSION = "1.1.0";
     public static final String WELCOME_MESSAGE = """
             Welcome, Mayor!
             Your mission is to build the perfect city.
